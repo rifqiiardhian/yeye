e880eae3456ad25d5911942a69c464fc7001d215
berliana123 <60633776+berliana123@users.noreply.github.com>
2020-05-09 21:09:39 +0700

    Update Contact

---
+++ resources/views/admin/contact_detail.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="au theme template">
+    <meta name="author" content="Hau Nguyen">
+    <meta name="keywords" content="au theme template">
+
+    <!-- Title Page-->
+    <title>{{ $title }}</title>
+    @include('admin/include/css')
+</head>
+
+<body class="animsition">
+    <div class="page-wrapper">
+
+        @include('admin/include/header')
+
+        <!-- WELCOME-->
+        <section class="welcome2 p-t-40 p-b-55">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="welcome2-inner m-t-60">
+                            <div class="welcome2-greeting">
+                                <h1 class="title-6">{{ $welcome_title }}</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="au-breadcrumb3">
+                            <div class="au-breadcrumb-left">
+                                <ul class="list-unstyled list-inline au-breadcrumb__list">
+                                    <li class="list-inline-item active">
+                                        <a href="#">Admin</a>
+                                    </li>
+                                    <li class="list-inline-item seprate">
+                                        <span>/</span>
+                                    </li>
+                                    <li class="list-inline-item">{{ $breadcrumb }}</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- END WELCOME-->
+
+        <!-- PAGE CONTENT-->
+        <div class="page-container3">
+            <section class=" p-t-70 p-b-70">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-3">
+                            @include('admin/include/menu')
+                        </div>
+                        <div class="col-xl-9">
+                            <div class="page-content">
+                                <!-- MAIN CONTENT -->
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="card">
+                                            @foreach($kontak as $data)
+                                            <div class="row card-body card-block">
+                                                <div class="col-6">
+                                                    <h3>{{ $data->subjek }}</h3>
+                                                    <p>Nama : {{($data->nama) }}<p>
+                                                    <p>Email : {{ $data->email }}</p>
+                                                    <p>No Telepon : {{ $data->telepon }}</p>
+                                                    <p class="text-justify mt-5"> Isi Pesan : <p>
+                                                    <p class="text-justify mt-3">{{ $data->pesan }}</p>
+                                                </div>
+                                            </div>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- END MAIN CONTENT -->
+
+                                <!-- FOOTER -->
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        @include('admin/include/footer')
+                                    </div>
+                                </div>
+                                <!-- END FOOTER -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- END PAGE CONTENT  -->
+    </div>
+
+    @include('admin/include/javascript')
+
+</body>
+
+</html>
+<!-- end document-->
